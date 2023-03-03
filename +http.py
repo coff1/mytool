5c95c6845eb501a1cb76f90d495d9a30585e79aa
bamb000 <573281527@qq.com>
2023-03-03 09:56:13 +0800

    '增加功能'

--- +http.py
+++ +http.py
@@ -1,15 +1,40 @@
-import sys
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode","-m",help="指定添加的协议 http/https/all(http and https)...或者其他特定的",required=False,default='http')
+parser.add_argument("--input","-i",help="指定含输入内容的文件",required=True)
+parser.add_argument("--output","-o",help="指定输出的文件名",required=False)
+parser.add_argument("--path","-p",help="生成的url路径,默认为'/'",required=False,default="/")
 
-f = open(sys.argv[1],'r', encoding='UTF-8')
-sub = [line.strip() for line in f.readlines()]
-f.close()
-for i in sub:
-	if 'http' not in i:
-		url = 'http://'+i+'/'
-		urls = 'https://'+i+'/'
-		with open(sys.argv[2], 'a') as o:
-			o.write(url)
-			o.write("\n")
-			# o.write(urls)
-			# o.write("\n")
-			o.close()
+args=parser.parse_args()
+
+
+f_input=open(args.input,'r',encoding='utf-8')
+date_input = list()
+
+for i in f_input.readlines():
+    if i.strip() not in date_input:
+        date_input.append(i.strip())
+f_input.close()
+        
+
+
+
+result=""
+
+if args.mode == 'all':
+    for i in date_input:
+        result+='https://'+i+args.path+'\n'
+        result+='http://'+i+args.path+'\n'
+    
+else:
+	for i in date_input:
+		result+=args.mode+'://'+i+args.path+'\n'
+		result+=args.mode+'://'+i+args.path+'\n'
+                
+print(result)
+
+if args.output:
+    f_output = open(args.output,'w',encoding='utf-8')     
+    f_output.write(result)     
+    f_output.close()
+    print("down! Results are saved in {}".format(args.output))
