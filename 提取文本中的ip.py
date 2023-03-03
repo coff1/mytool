335129618b862a1299e18cc4f4db14b663506da1
bamb000 <573281527@qq.com>
2023-03-03 09:42:45 +0800

    增强交互

--- 提取文本中的ip.py
+++ 提取文本中的ip.py
@@ -1,8 +1,21 @@
 import re,sys
-f=open(sys.argv[1])
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--input","-i",help="输入文件",required=True)
+parser.add_argument("--output","-o",help="输出文件",required=False)
+args=parser.parse_args()
+
+
+
+f=open(args.input,encoding='utf-8')
 data=f.read()
 f.close()
 
 ip=re.findall(r"[\d]+[.][\d]+[.][\d]+[.][\d]+",data)
-for i in ip:
-    print(i) 
+ip=list(set(ip))
+
+if args.output:
+    ip = ('\n').join(ip)
+    f = open(args.output,'w',encoding='utf-8')
+    f.write(ip)
+    f.close()
